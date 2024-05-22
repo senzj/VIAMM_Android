c619d523662ea0669b4c2a4868eeeb1b0c064d11
senzj <soulcrepzyt@gmail.com>
2024-05-23 00:58:56 +0800

    Order Activity
    Next up Navbar/ ActionBar

--- app/src/main/java/com/example/viamm/MainActivity.kt
+++ app/src/main/java/com/example/viamm/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.viamm.databinding.ActivityMainBinding
@@ -12,8 +13,11 @@ import com.example.viamm.storage.SharedData
 
 class MainActivity : AppCompatActivity() {
 
+//    Initializing variables
+    private lateinit var toolbar: Toolbar
     private lateinit var binding: ActivityMainBinding
-    lateinit var logoutBtn : Button
+    private lateinit var logoutBtn: Button
+    private lateinit var orderBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,18 +28,41 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Initialize the binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Set ups toolbar
+//        toolbar = findViewById(R.id.actionbar)
+//        setSupportActionBar(toolbar)
+
+        // Initialize buttons
+        orderBtn = findViewById(R.id.btn_order)
         logoutBtn = findViewById(R.id.btn_logout)
 
-        logoutBtn.setOnClickListener{
+        // Set onClickListeners
+        orderBtn.setOnClickListener {
+            redirectToOrder()
+        }
+
+        logoutBtn.setOnClickListener {
             logout() // Call the logout function
         }
 
-    }
+        // Set insets listener
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
-    // Function to handle logout
+    }
+//end of on create
+
+// V Other functions go here V
+
+// Function to handle logout
     private fun logout() {
         SharedData.getInstance(this).isLoggedIn = false
 
@@ -46,13 +73,20 @@ class MainActivity : AppCompatActivity() {
         finish() // Finish the current activity
     }
 
-    //Other functions go here
 
-    //Function to check if user is logged in
+//    Redirect to order activity
+    private fun redirectToOrder() {
+        val intent = Intent(applicationContext, OrderActivity::class.java)
+        startActivity(intent)
+    }
+
+
+
+    // Function to check if user is logged in
     override fun onStart() {
         super.onStart()
 
-        if(!SharedData.getInstance(this).isLoggedIn){
+        if (!SharedData.getInstance(this).isLoggedIn) {
             val intent = Intent(applicationContext, LoginActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
